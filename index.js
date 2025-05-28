b6f8ce0be2d89262db910eeb5baf6183bc759d89
Rutvik Patel <rutvikpatel3377@gmail.com>
2025-05-28 23:42:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From dhruv" }');
+    res.send('{ "response": "Hello From rutvik" }');
 });
 
 app.get('/will', function (req, res) {
